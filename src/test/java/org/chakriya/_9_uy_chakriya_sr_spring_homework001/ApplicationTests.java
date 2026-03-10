10244722dafff0ee6f8ffef4278334a3f709eaab
Uy-Chakriya <uychakriya002@gmail.com>
2026-03-10 19:38:39 +0700

    Initial commit

---
+++ src/test/java/org/chakriya/_9_uy_chakriya_sr_spring_homework001/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chakriya._9_uy_chakriya_sr_spring_homework001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
